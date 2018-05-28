9f2d85fd7b5f8be03bbf0fe14abdb980bc7395bb
ShreeSorna <39579992+ShreeSorna@users.noreply.github.com>
2018-05-28 06:11:25 +0000

    Update build_docker_image.bat

--- test/build_docker_image.bat
+++ test/build_docker_image.bat
@@ -1,4 +1,3 @@
-cd C:\Users\tfssetup\Desktop\docker
 docker build -t testapp_%BUILD_NUMBER% .
 docker tag testapp_%BUILD_NUMBER% deploymentimagerepo.azurecr.io/testapp_%BUILD_NUMBER%
 docker login deploymentimagerepo.azurecr.io -u DeploymentImageRepo -p GoeYzos9LWqguT8MyW6jNiUgPdEM3D7=
